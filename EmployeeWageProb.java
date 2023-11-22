6393de03cba15833a7deade3ee2b48d978e2c1ad
Anurag <111561835+roarscarch@users.noreply.github.com>
2023-11-22 06:55:59 +0530

    refactored code + used switch instead of if else

--- EmployeeWageProb.java
+++ EmployeeWageProb.java
@@ -2,33 +2,34 @@ public class EmployeeWageProb {
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage Calculation Problem");
         System.out.println("Check if Employee is present or absent");
-        int emp_Present_FullTime = 1;
-        int emp_Present_PartTime= 2;
 
-        int empHrs =0;
-        int empWage =0;
-        int emp_wage_per_hour = 20;
-        
-        
-        double empCheck = Math.floor(Math.random() * 10) % 3;
-       
-        if(empCheck == emp_Present_FullTime){
-            System.out.println("Employee is Present");
-            empHrs = 8;
+        final int empPresentFullTime = 1;
+        final int empPresentPartTime = 2;
+
+        int empHrs = 0;
+        int empWage = 0;
+        int empWagePerHour = 20;
+
+        int empCheck = (int) Math.floor(Math.random() * 10) % 3;
+
+        switch (empCheck) {// in java , in switch u can only add constant expressions , and final = const in java
+            case empPresentFullTime:
+                System.out.println("Employee is present Full Time:");
+                empHrs = 8;
+                break;
+            case empPresentPartTime:
+                System.out.println("Employee is Doing Part Time:");
+                empHrs = 4;
+                break;
+            default:
+                System.out.println("Employee is Absent:");
+                empHrs = 0;
+                break;
         }
-        else if(empCheck == emp_Present_PartTime) {
-            System.out.println("Employee is Halftime");
-            empHrs = 4;
-        }
-        else{
-            System.out.println("Employee is Absent");
-            empHrs =0;
-        }
-        //calculation
-        empWage = empHrs * emp_wage_per_hour;
-        System.out.println("Emp Hours : "+empHrs);
-        System.out.println("Emp Wage : "+empWage);
+
+        // Calculation and print statements outside the switch
+        empWage = empHrs * empWagePerHour;
+        System.out.println("Emp Hours : " + empHrs);
+        System.out.println("Emp Wage : " + empWage);
     }
 }
-
-
