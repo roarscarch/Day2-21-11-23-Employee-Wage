d0700dc3ff7298466996658ba468d6adc0b120c1
Anurag <111561835+roarscarch@users.noreply.github.com>
2023-11-22 07:07:59 +0530

    wage for a month

--- EmployeeWageProb.java
+++ EmployeeWageProb.java
@@ -1,33 +1,27 @@
 public class EmployeeWageProb {
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage Calculation Problem");
-        System.out.println("Check if Employee is present or absent");
-
-        final int empPresentFullTime = 1;
+        final int empPresentFullTime = 1;// for switch
         final int empPresentPartTime = 2;
 
         int empHrs = 0;
         int empWage = 0;
         int empWagePerHour = 20;
 
-        int empCheck = (int) Math.floor(Math.random() * 10) % 3;
+        for (int i = 0; i < 20; i++) {
+            int empCheck = (int) Math.floor(Math.random() * 10) % 3;
 
-        switch (empCheck) {// in java , in switch u can only add constant expressions , and final = const in java
-            case empPresentFullTime:
-                System.out.println("Employee is present Full Time:");
-                empHrs = 8;
-                break;
-            case empPresentPartTime:
-                System.out.println("Employee is Doing Part Time:");
-                empHrs = 4;
-                break;
-            default:
-                System.out.println("Employee is Absent:");
-                empHrs = 0;
-                break;
+            switch (empCheck) {
+                case empPresentFullTime:
+                    empHrs += 8;
+                    break;
+                case empPresentPartTime:
+                    empHrs += 4;
+                    break;
+            }
         }
 
-        // Calculation and print statements outside the switch
+        
         empWage = empHrs * empWagePerHour;
         System.out.println("Emp Hours : " + empHrs);
         System.out.println("Emp Wage : " + empWage);
