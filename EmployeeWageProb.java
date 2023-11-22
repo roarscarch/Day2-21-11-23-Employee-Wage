1fc30a7d8be99e5801fce0ea3df554565beed859
Anurag <111561835+roarscarch@users.noreply.github.com>
2023-11-22 05:42:28 +0530

    if employee is absent or present

--- EmployeeWageProb.java
+++ EmployeeWageProb.java
@@ -1,5 +1,14 @@
 public class EmployeeWageProb {
     public static void main(String[] args) {
-        System.out.println("WELCOME TO EMPLOYEE WAGE COMPUTATION PROGRAMME");
+        System.out.println("Welcome to Employee Wage Calculation Problem");
+        System.out.println("Check if Employee is present or absent");
+        int empPresesent = 1;
+        double empCheck = Math.floor(Math.random() * 10) % 2;
+        if(empCheck == empPresesent){
+            System.out.println("Employee is Present");
+        }
+        else {
+            System.out.println("Employee is Absent");
+        }
     }
 }
