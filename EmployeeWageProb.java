6d94904683dc6256695bcc1fb0f7e077b61bd194
Anurag <111561835+roarscarch@users.noreply.github.com>
2023-11-25 00:57:53 +0530

    multiple companies

--- EmployeeWageProb.java
+++ EmployeeWageProb.java
@@ -1,24 +1,15 @@
+import java.util.Scanner;
 import java.util.Random;
 
 public class EmployeeWageCalculator {
     private static final int EMP_PRESENT_FULL_TIME = 1;
     private static final int EMP_PRESENT_PART_TIME = 2;
 
-    private int empHrs;
-    private int empWagePerHour;
-    private int empDays;
+    public static void computeEmployeeWage(String companyName, int empWagePerHour, int workingDays, int workingHoursPerMonth) {
+        int empHrs = 0;
+        int empDays = 0;
 
-    public EmployeeWageCalculator(int empWagePerHour) {
-        this.empWagePerHour = empWagePerHour;
-    }
-
-    private int generateEmployeeCheck() {
-        Random random = new Random();
-        return random.nextInt(3); // Returns 0, 1, or 2
-    }
-
-    public void computeEmployeeWage() {
-        while (empHrs < 100 && empDays < 20) {
+        while (empHrs < workingHoursPerMonth && empDays < workingDays) {
             int empCheck = generateEmployeeCheck();
 
             switch (empCheck) {
@@ -32,20 +23,39 @@ public class EmployeeWageCalculator {
                     break;
             }
         }
-    }
 
-    public void displayEmployeeDetails() {
         int empWage = empHrs * empWagePerHour;
+        System.out.println("Company: " + companyName);
         System.out.println("Emp Hours: " + empHrs);
         System.out.println("Emp Days: " + empDays);
         System.out.println("Emp Wage: " + empWage);
+        System.out.println("-------------");
+    }
+
+    private static int generateEmployeeCheck() {
+        Random random = new Random();
+        return random.nextInt(3); // Returns 0, 1, or 2
     }
 
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage Calculation Problem");
 
-        EmployeeWageCalculator calculator = new EmployeeWageCalculator(20);
-        calculator.computeEmployeeWage();
-        calculator.displayEmployeeDetails();
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter Company Name: ");
+        String companyName = scanner.nextLine();
+
+        System.out.print("Enter Wage Per Hour: ");
+        int empWagePerHour = scanner.nextInt();
+
+        System.out.print("Enter Working Days: ");
+        int workingDays = scanner.nextInt();
+
+        System.out.print("Enter Working Hours Per Month: ");
+        int workingHoursPerMonth = scanner.nextInt();
+
+        computeEmployeeWage(companyName, empWagePerHour, workingDays, workingHoursPerMonth);
+
+        scanner.close();
     }
 }
